31b41ae8170154edabb6f811312858e29b630cbb
Daniel Lima <dnlima77237@gmail.com>
2024-08-30 22:53:15 -0300

    Added function docstring

--- filesort/config.py
+++ filesort/config.py
@@ -1,4 +1,3 @@
-
 # As extensões foram selecionadas através de fileinfo.com
 categories: dict[str, list[str]] = {
     "Executable": ["apk", "exe", "jar", "bat", "bin", "appimage", "sh"],
@@ -6,12 +5,47 @@ categories: dict[str, list[str]] = {
     "Video": ["mkv", "aep", "mp4", "m4v", "mov"],
     "Image": ["jpeg", "png", "svg"],
     "Document": ["doc", "docx", "odt", "tex", "txt", "md", "pptx", "ppt"],
-    "Data": ["db", "sqlite","sql","odb","xlsx","xls","dat","csv"],
-    "Compressed": ["7z", "deb", "gz", "pkg", "rar", "rpm", "tar.gz", "tar.bz2", "tar", "xapk", "zip", "zipx"],
+    "Data": ["db", "sqlite", "sql", "odb", "xlsx", "xls", "dat", "csv"],
+    "Compressed": [
+        "7z",
+        "deb",
+        "gz",
+        "pkg",
+        "rar",
+        "rpm",
+        "tar.gz",
+        "tar.bz2",
+        "tar",
+        "xapk",
+        "zip",
+        "zipx",
+    ],
 }
 
-def create_file_extension_dict(categories: dict[str, list[str]]) -> dict[str, str]:
-    return {extension : category for category, extensions in categories.items() for extension in extensions}
+
+def create_file_extension_dict(file_categories: dict[str, list[str]]) -> dict[str, str]:
+    """
+    Formata as categorias em um dicionário que segue este padrão:
+
+    "rpm" : "Compressed",
+    "png" : "Image",
+    ...
+
+    Args:
+        categories (dict[str, list[str]]): Um dicionário que define as categorias
+        para cada tipo de arquivo.
+
+    Returns:
+        dict[str, str]: Um dicionário em que as chaves são as extensões dos
+        arquivos e os valores são a sua categoria.
+
+    """
+    return {
+        extension: category
+        for category, extensions in file_categories.items()
+        for extension in extensions
+    }
+
 
 if __name__ == "__main__":
     print(create_file_extension_dict(categories))

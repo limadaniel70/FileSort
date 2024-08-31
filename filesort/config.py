19234724ae6ab402b14c5bb48a63ec6e12dcd8ef
Daniel Lima <dnlima77237@gmail.com>
2024-08-30 22:46:47 -0300

    Improved config

--- filesort/config.py
+++ filesort/config.py
@@ -1,62 +1,17 @@
-executable_files: dict[str, str] = {
-    "apk": "Executable",
-    "exe": "Executable",
-    "jar": "Executable",
-    "bat": "Executable",
-    "bin": "Executable",
-    "appimage": "Executable",
-    "sh": "Executable",
+
+# As extensões foram selecionadas através de fileinfo.com
+categories: dict[str, list[str]] = {
+    "Executable": ["apk", "exe", "jar", "bat", "bin", "appimage", "sh"],
+    "Audio": ["mp3", "m4a", "wav"],
+    "Video": ["mkv", "aep", "mp4", "m4v", "mov"],
+    "Image": ["jpeg", "png", "svg"],
+    "Document": ["doc", "docx", "odt", "tex", "txt", "md", "pptx", "ppt"],
+    "Data": ["db", "sqlite","sql","odb","xlsx","xls","dat","csv"],
+    "Compressed": ["7z", "deb", "gz", "pkg", "rar", "rpm", "tar.gz", "tar.bz2", "tar", "xapk", "zip", "zipx"],
 }
 
-media_files: dict[str, str] = {
-    "mp3": "Audio",
-    "m4a": "Audio",
-    "wav": "Audio",
-    "mkv": "Video",
-    "aep": "Video",
-    "mp4": "Video",
-    "m4v": "Video",
-    "mov": "Video",
-    "jpeg": "Image",
-    "png": "Image",
-    "svg": "Image",
-}
+def create_file_extension_dict(categories: dict[str, list[str]]) -> dict[str, str]:
+    return {extension : category for category, extensions in categories.items() for extension in extensions}
 
-text_document_files: dict[str, str] = {
-    "doc": "Document",
-    "docx": "Document",
-    "odt": "Document",
-    "tex": "Document",
-    "txt": "Document",
-    "md": "Document",
-    "pptx": "Document",
-    "ppt": "Document",
-}
-
-data_files: dict[str, str] = {
-    "db": "Data",
-    "sqlite": "Data",
-    "sql": "Data",
-    "odb": "Data",
-    "xlsx": "Data",
-    "xls": "Data",
-    "dat": "Data",
-    "csv": "Data",
-}
-
-compressed_files: dict[str, str] = {
-    "7z": "Compressed",
-    "deb": "Compressed",
-    "gz": "Compressed",
-    "pkg": "Compressed",
-    "rar": "Compressed",
-    "rpm": "Compressed",
-    "tar.gz": "Compressed",
-    "tar.bz2": "Compressed",
-    "tar": "Compressed",
-    "xapk": "Compressed",
-    "zip": "Compressed",
-    "zipx": "Compressed",
-}
-
-files: dict[str, str] = executable_files | media_files | text_document_files | data_files | compressed_files
+if __name__ == "__main__":
+    print(create_file_extension_dict(categories))

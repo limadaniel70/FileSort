b6b244470e950547741def6c746f5854b8238028
Daniel Lima <dnlima77237@gmail.com>
2024-09-13 12:09:17 -0300

    Fixed identation

--- filesort/config.py
+++ filesort/config.py
@@ -24,34 +24,15 @@
 Defines the category for each file extension specified.
 """
 categories: dict[str, list[str]] = {
+    # fmt: off
     "Executable": [".apk", ".exe", ".jar", ".bat", ".bin", ".appimage", ".sh"],
     "Audio": [".mp3", ".m4a", ".wav"],
     "Video": [".mkv", ".aep", ".mp4", ".m4v", ".mov"],
     "Image": [".jpeg", ".jpg", ".png", ".svg", ".gif", ".webp"],
-    "Document": [
-        ".doc",
-        ".docx",
-        ".odt",
-        ".tex",
-        ".txt",
-        ".md",
-        ".pptx",
-        ".ppt",
-        ".pdf",
-    ],
+    "Document": [".doc", ".docx", ".odt", ".tex", ".txt", ".md", ".pptx", ".ppt", ".pdf"],
     "Data": [".db", ".sqlite", ".sql", ".odb", ".xlsx", ".xls", ".dat", ".csv"],
-    "Compressed": [
-        ".7z",
-        ".deb",
-        ".gz",
-        ".pkg",
-        ".rar",
-        ".rpm",
-        ".gz",
-        ".bz2",
-        ".tar",
-        ".zip",
-    ],
+    "Compressed": [".7z", ".deb", ".gz", ".pkg",".rar", ".rpm", ".gz", ".bz2", ".tar", ".zip"],
+    # fmt: on
 }
 
 
